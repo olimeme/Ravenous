bda624df721517122bfc01247d9610842b53460c
Alibek <alibek.mem@mail.ru>
2020-08-25 21:46:47 +0600

    App.js: App() function changed to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,16 @@ import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
 }
 
 export default App;
