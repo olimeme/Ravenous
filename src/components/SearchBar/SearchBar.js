c13dbd479ef532edf092adbcc54128dc4979a021
Alibek <alibek.mem@mail.ru>
2020-08-29 12:12:30 +0600

    SearchBar.js: handle Term or Location change (onChange)

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,6 +11,8 @@ class SearchBar extends React.Component{
             location:'',
             sortBy: 'best_match'
         };
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
         this.sortByOptions ={
             "Best Match": 'best_match',
             "Highest Rated": 'rating',
@@ -52,8 +54,8 @@ class SearchBar extends React.Component{
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                    <input onChange={this.handleLocationChange} placeholder="Where?" />
                 </div>
                 <div className="SearchBar-submit">
                     <a>Let's Go</a>
