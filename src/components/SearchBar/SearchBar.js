6fb319b9611f7570cc3c80da16d644a8aed0a98f
Alibek <alibek.mem@mail.ru>
2020-08-29 11:54:48 +0600

    SearchBar.js: handels sorting option change (on click)

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -29,7 +29,10 @@ class SearchBar extends React.Component{
     renderSortByOptions(){
         return Object.keys(this.sortByOptions).map(sortByOption => {
             const sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li key={sortByOptionValue} >{sortByOption}</li>
+            return <li key={sortByOptionValue} 
+                       className={this.getSortByClass(sortByOptionValue)}
+                       onClick={this.handleSortByChange.bind(this,sortByOptionValue)}
+                       >{sortByOption}</li>
         });
     }
     render(){
